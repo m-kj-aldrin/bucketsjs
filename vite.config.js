e33c14538eeb930d55f8827d58c3a34e88550378
m-kj-aldrin <mkj.aldrin@icloud.com>
2024-11-06 15:53:44 +0100

    Complete rewrite, instead of creating request animation frame instances to keep track of value, poll the calculated value based on elapsed time since last poll

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,18 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.js"),
-      name: "buckets",
-      fileName: () => "index.js", // Ensures the output file is always named index.js
-      formats: ["es"], // Output only the ES module format
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.js"),
+            name: "buckets",
+            fileName: () => "index.js",
+            formats: ["es"],
+        },
+        rollupOptions: {
+            external: [],
+            output: {
+                globals: {},
+            },
+        },
     },
-    rollupOptions: {
-      // Externalize dependencies that shouldn't be bundled
-      external: [],
-      output: {
-        globals: {},
-      },
-    },
-  },
 });
